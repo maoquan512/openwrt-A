a6ad68344af9766a05c92fddebb0d86fa16c7fbd
pinban <fanrongkuye@gmail.com>
2020-03-26 21:14:05 +0800

    chonglai

---
+++ diy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tindy2013/openwrt-subconverter.git package/openwrt-subconverter
+./scripts/feeds update -a
+./scripts/feeds install -a
