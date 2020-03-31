8979c4be7c05322dbc49d1df3d15f37310bdb2d6
pinban <fanrongkuye@gmail.com>
2020-04-01 01:31:57 +0800

    jiaruopem

--- diy.sh
+++ diy.sh
@@ -8,5 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tindy2013/openwrt-subconverter.git package/openwrt-subconverter
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
