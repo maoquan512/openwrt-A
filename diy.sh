6d79b74af0ab675686f954f507e12d3859490cc1
maoquan512 <43471562+maoquan512@users.noreply.github.com>
2020-04-08 11:29:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tindy2013/openwrt-subconverter.git package/openwrt-subconverter
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/maoquan512/neither/raw/master/clash package/OpenClash/luci-app-openclash/files/etc/openclash
-chmod 777 clash package/OpenClash/luci-app-openclash/files/etc/openclash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
