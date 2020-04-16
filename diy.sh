57923cd3b4f401416c3b2140792def8dd83693bc
maoquan512 <43471562+maoquan512@users.noreply.github.com>
2020-04-16 15:01:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/fw876/helloworld package/helloworld
 git clone https://github.com/tindy2013/openwrt-subconverter.git package/openwrt-subconverter
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 ./scripts/feeds update -a
